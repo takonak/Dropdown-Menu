c4c1a03d3a7d86be6a6ff8eabb0db52c1bd2fec9
Tamar Nakashidze <120483541+takonak@users.noreply.github.com>
2024-01-16 22:28:13 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
